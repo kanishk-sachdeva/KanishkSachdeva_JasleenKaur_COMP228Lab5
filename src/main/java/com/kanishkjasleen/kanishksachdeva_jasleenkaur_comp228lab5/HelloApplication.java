556996fafbaf3c5954bb8f0fa6de15a711bd63e4
Kanishk Sachdeva <kskanu2020@gmail.com>
2021-11-28 11:28:37 -0500

    Updated

--- src/main/java/com/kanishkjasleen/kanishksachdeva_jasleenkaur_comp228lab5/HelloApplication.java
+++ src/main/java/com/kanishkjasleen/kanishksachdeva_jasleenkaur_comp228lab5/HelloApplication.java
@@ -138,6 +138,10 @@ public class HelloApplication extends Application {
 
                     if (rowsInserted > 0) {
                         System.out.println("A new player was inserted successfully!");
+                        Alert alert = new Alert(Alert.AlertType.NONE);
+                        alert.setAlertType(Alert.AlertType.INFORMATION);
+                        alert.setContentText("A new Player has been added");
+                        alert.show();
                         stage.setScene(Dashboard(conn));
                     }
 
@@ -203,6 +207,10 @@ public class HelloApplication extends Application {
 
                     if (gameInserted > 0) {
                         System.out.println("A new game was inserted successfully!");
+                        Alert alert = new Alert(Alert.AlertType.NONE);
+                        alert.setAlertType(Alert.AlertType.INFORMATION);
+                        alert.setContentText("A new Game has been added");
+                        alert.show();
                         stage.setScene(newGameForm(conn));
                     }
 
@@ -401,6 +409,12 @@ public class HelloApplication extends Application {
 
                     if (rowsInserted > 0) {
                         System.out.println("A new record was inserted successfully!");
+
+                        Alert alert = new Alert(Alert.AlertType.NONE);
+                        alert.setAlertType(Alert.AlertType.INFORMATION);
+                        alert.setContentText("Player match has been recorded");
+                        alert.show();
+
                         stage.setScene(Dashboard(conn));
                     }
 
@@ -438,22 +452,65 @@ public class HelloApplication extends Application {
         content.setPadding(new Insets(2));
 
         TableView table = new TableView();
+        table.setMinWidth(240);
         table.setEditable(false);
 
-        TableColumn<Player, String> column1 = new TableColumn<>("First Name");
-        column1.setCellValueFactory(new PropertyValueFactory<>("name"));
+        TableColumn<Player, String> column1 = new TableColumn<>("Player Name");
+        column1.setCellValueFactory(new PropertyValueFactory<>("player_name"));
+        TableColumn<Player, String> column2 = new TableColumn<>("Address");
+        column2.setCellValueFactory(new PropertyValueFactory<>("address"));
+        TableColumn<Player, String> column3 = new TableColumn<>("Game Played");
+        column3.setCellValueFactory(new PropertyValueFactory<>("gamename"));
+        TableColumn<Player, String> column4 = new TableColumn<>("Score");
+        column4.setCellValueFactory(new PropertyValueFactory<>("score"));
+        TableColumn<Player, String> column5 = new TableColumn<>("Date Played");
+        column5.setCellValueFactory(new PropertyValueFactory<>("date"));
+        TableColumn<Player, String> column6 = new TableColumn<>("Phone number");
+        column6.setCellValueFactory(new PropertyValueFactory<>("phonenumber"));
 
 
-        TableColumn<Player, String> column2 = new TableColumn<>("Last Name");
-        column2.setCellValueFactory(new PropertyValueFactory<>("email"));
-        TableColumn<Player, String> column3 = new TableColumn<>("Last Name");
-        column3.setCellValueFactory(new PropertyValueFactory<>("game"));
+        table.getColumns().addAll(column1, column2, column3, column4, column5, column6);
 
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM PLAYERANDGAME");
+            while (resultSet.next()){
+                String name = "";
+                String address = "";
+                int phonenumber = 0;
 
-        table.getColumns().addAll(column1, column2, column3);
-        table.getItems().add(new Player("hi","hi","ijio"));
-        table.getItems().add(new Player("hi","hi","ijio"));
-        table.getItems().add(new Player("hi","hi","ijio"));
+                try {
+                    Statement stm1 = conn.createStatement();
+                    ResultSet rs1 = stm1.executeQuery("SELECT * FROM PLAYER WHERE player_id = " + resultSet.getString(3));
+                    while (rs1.next()){
+                        name = rs1.getString(2) + " " + rs1.getString(3);
+                        address = rs1.getString(4) + ", " + rs1.getString(6) + ", " + rs1.getString(5);
+                        phonenumber = rs1.getInt(7);
+                    }
+                }catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                String game_title = "";
+                try {
+                    Statement stm2 = conn.createStatement();
+                    ResultSet rs2 = stm2.executeQuery("SELECT * FROM GAME WHERE game_id = " + resultSet.getString(2));
+                    while (rs2.next()){
+                        game_title = rs2.getString(2);
+                    }
+                }catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                table.getItems().add(new Player(name,address,game_title,resultSet.getString(5), resultSet.getString(4), phonenumber));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+//        table.getItems().add(new Player("hi","hi","ijio","10","26 nov",65565));
+//        table.getItems().add(new Player("hi","hi","ijio","10","26 nov",65565));
+//        table.getItems().add(new Player("hi","hi","ijio","10","26 nov",65565));
         content.getChildren().add(table);
 
         panel.setBody(content);
@@ -465,36 +522,64 @@ public class HelloApplication extends Application {
     }
 
     public class Player {
-        public String name, email, game;
+        String player_name, address, gamename, score, date;
+        int phonenumber;
 
-        public Player(String name, String email, String game) {
-            this.name = name;
-            this.email = email;
-            this.game = game;
+        public Player(String player_name, String address, String gamename, String score, String date, int phonenumber) {
+            this.player_name = player_name;
+            this.address = address;
+            this.gamename = gamename;
+            this.score = score;
+            this.date = date;
+            this.phonenumber = phonenumber;
         }
 
-        public String getName() {
-            return name;
+        public String getPlayer_name() {
+            return player_name;
         }
 
-        public void setName(String name) {
-            this.name = name;
+        public void setPlayer_name(String player_name) {
+            this.player_name = player_name;
         }
 
-        public String getEmail() {
-            return email;
+        public String getAddress() {
+            return address;
         }
 
-        public void setEmail(String email) {
-            this.email = email;
+        public void setAddress(String address) {
+            this.address = address;
         }
 
-        public String getGame() {
-            return game;
+        public String getGamename() {
+            return gamename;
         }
 
-        public void setGame(String game) {
-            this.game = game;
+        public void setGamename(String gamename) {
+            this.gamename = gamename;
+        }
+
+        public String getScore() {
+            return score;
+        }
+
+        public void setScore(String score) {
+            this.score = score;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public int getPhonenumber() {
+            return phonenumber;
+        }
+
+        public void setPhonenumber(int phonenumber) {
+            this.phonenumber = phonenumber;
         }
     }
 
